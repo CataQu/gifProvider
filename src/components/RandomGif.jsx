ce89b8c2ca4e10cbb3708b8500f5d333393f371f
CataQu <cataquarleri@gmail.com>
2022-03-31 13:54:07 -0500

    acomodo estilo y vistas de randomGif y randomTrendingGif

--- src/components/RandomGif.jsx
+++ src/components/RandomGif.jsx
@@ -1,33 +1,19 @@
 
 
-const RandomGif = ({randomTrendingGif, randomGif}) => {
+const RandomGif = ({randomGif}) => {
     return (
         <>
-              {/* RANDOM GIF - Si el state randomReady es true, renders el randomGif */}
-          <div className="col-lg-3 col-md-6 mb-4" style={{margin: "auto"}}>
+
+        <div className="col-lg-3 col-md-6 mb-4">
           <div className="card h-100" style={{border: "red 3px solid"}}> 
-          { /*Un borde rojo para diferenciarlo */}
+          <h2>Random Gif</h2>
             <img className="card-img-top"
-              src={randomTrendingGif.url}/>
-             { console.log("RANDOM TRENDNIG!!!!", randomTrendingGif)}
-            <div className="card-body">
-              <h4 className="card-title">
-                {randomTrendingGif.title}</h4>
-            </div>
-          </div>
+            src={randomGif.url}/>
+        <div className="card-body">
+            <h4 className="card-title">
+            {randomGif.title}</h4>
         </div>
-          {/* RANDOM GIF - Si el state randomReady es true, renders el randomGif */}
-          <div className="col-lg-3 col-md-6 mb-4" style={{margin: "auto"}}>
-          <div className="card h-100" style={{border: "red 3px solid"}}> 
-          { /*Un borde rojo para diferenciarlo */}
-            <img className="card-img-top"
-              src={randomGif.url}/>
-             { console.log("RANDOM GIF!!!!", randomGif)}
-            <div className="card-body">
-              <h4 className="card-title">
-                {randomGif.title}</h4>
-            </div>
-          </div>
+        </div>
         </div>
         </>
     );
