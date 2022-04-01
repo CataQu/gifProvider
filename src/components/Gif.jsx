316f4858b4151d00e8f825b57438416ddb3d6b09
Joaquin Caggiano <joaquincaggiano@gmail.com>
2022-04-01 13:18:23 -0300

    terminamos los 8 gifs random

--- src/components/Gif.jsx
+++ src/components/Gif.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
-const Gifs = ({gif, randomReady, randomGif}) => {
+const Gifs = ({gif, arrayDe8}) => {
   let contenido;
-//   let numeroRandom =  Math.trunc(Math.random()*50)+1 ;
-//   console.log("NUMERO RANDOM!!!", numeroRandom)
-//   let nRrestado = (numeroRandom) => {if (numeroRandom >= 42){
-//    return nRrestado = numeroRandom - 8
-//   } else {
-//    return nRrestado = numeroRandom + 8
-//   }
-// }
-// console.log("NUMERO Restado!!!", nRrestado(numeroRandom))
-// let segundoIndice = nRrestado(numeroRandom)
-// console.log("Segundo indice", segundoIndice)
 
   if (gif == "") {
     contenido = <p>Cargando...</p>;
-  } 
+  } else if (arrayDe8 !== "") {
+    contenido = arrayDe8.map((oneGif, i) => {
+      return (
+        <div key={i} className="col-lg-3 col-md-6 mb-4">
+          <div className="card h-100">
+            <img
+              className="card-img-top"
+              src={oneGif.images.downsized.url}
+            />
+            <div className="card-body">
+              <h4 className="card-title">{oneGif.title}</h4>
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
   else {
     contenido = gif.map((oneGif, i) => {
-      
+      console.log("array de 8 en GIF", arrayDe8)
       return (
-        // i<nRrestado(numeroRandom) && i>=numeroRandom && (
           i<8 && (
         <div key={i} className="col-lg-3 col-md-6 mb-4">
           <div className="card h-100">
